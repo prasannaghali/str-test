1dcccb5e5e79a5f66ae368051be0609cc4f00ed1
prasannaghali <pghali@gmail.com>
2015-05-16 23:03:48 +0800

    Move semantics added
    Move ctor, move assignment, and operator+ implemented.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include "str.h"
 
+Str foo() {
+	//return std::move(Str("foo"));
+	return Str("foo");
+}
+
+Str goo() {
+	//return std::move(Str("goo"));
+	return Str("goo");
+}
+
 int main() {
-	Str s1{"abc"}, s2{"xyz"}, s3;
+	Str s1{'s','1'}, s2{'s','2'}, s3;
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
+	std::cout << "--------------test1-------------\n";
 	s3 = s1+s2;
-	std::cout << s3 << std::endl;
-	
-	s1 += s3;
-	std::cout << s1 << std::endl;
-	
-	Str s4{'m','n','o','p'};
-	std::cout << s4 << std::endl;
+	std::cout << "s3: " << s3 << std::endl;
+	std::cout << "--------------test2-------------\n";
+	s3 = s1+foo();
+	std::cout << "s3: " << s3 << std::endl;
+	std::cout << "--------------test3-------------\n";
+	s3 = foo()+s2;
+	std::cout << "s3: " << s3 << std::endl;
+	std::cout << "--------------test4-------------\n";
+	s3 = foo()+goo();
+	std::cout << "s3: " << s3 << std::endl;
+	std::cout << "--------------endtest-------------\n";
 }
